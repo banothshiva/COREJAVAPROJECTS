4669d1bc07e6e6d622c651e33e303a40eef7701a
banothshiva <banothshiva9052@gmail.com>
2024-03-20 23:21:20 +0530

    STRINGS VOWEL LATERS PRINT IN SYNTAX

---
+++ STRING_2/src/com/sathya/STRING_VOWELS.java
@@ -0,0 +1,21 @@
+package com.sathya;
+
+public class STRING_VOWELS 
+{
+
+	public static void main(String[] args) 
+	{
+		String name = "sathyatech";
+		for (int i = 0; i < name.length(); i++) 
+		{
+			char ch = name.charAt(i);
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			{
+				System.out.print(ch);
+			}
+			
+		}
+
+	}
+
+}
